58291ab2f9be2485b2e4933802d5ffc1c63cfbf5
rene van der ark <rene.vanderark@kb.nl>
2019-11-27 08:08:50 +0100

    adds javadoc in docs directory.

--- src/main/java/nl/kb/xml/transformassert/TransformAssert.java
+++ src/main/java/nl/kb/xml/transformassert/TransformAssert.java
@@ -10,12 +10,24 @@ import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
 import java.util.function.Consumer;
 
+/**
+ * Entrypoint for the transform-assert library.<br>
+ * Use the static method {@link #describe(File)} to start work.
+ */
 public class TransformAssert {
 
     private TransformAssert() {
 
     }
 
+    /**
+     * Declares the xslt file to be tested
+     * @param xsltFile the xslt {@link File}
+     * @param logBack custom {@link String} {@link Consumer} for log messages
+     * @param transformationOutput custom {@link String} {@link Consumer} for the xslt transformation output
+     * @return instance of {@link TransformAssertWithTransformer}
+     * @throws TransformerException when the xslt cannot be parsed by Saxon
+     */
     public static TransformAssertWithTransformer describe(File xsltFile, Consumer<String> logBack, Consumer<String> transformationOutput)
             throws TransformerException {
         final TransformAssertWithTransformer transformAssertWithTransformer =
@@ -27,6 +39,15 @@ public class TransformAssert {
         return transformAssertWithTransformer;
     }
 
+    /**
+     * Declares the xslt {@link String} to be tested
+     * @param xslt the xslt {@link String}
+     * @param logBack custom {@link String} {@link Consumer} for log messages
+     * @param transformationOutput custom {@link String} {@link Consumer} for the xslt transformation output
+     * @return instance of {@link TransformAssertWithTransformer}
+     * @throws UnsupportedEncodingException when the character set of the xslt {@link String} ia not supported
+     * @throws TransformerException when the xslt cannot be parsed by Saxon
+     */
     public static TransformAssertWithTransformer describe(String xslt, Consumer<String> logBack, Consumer<String> transformationOutput)
             throws UnsupportedEncodingException, TransformerException {
 
@@ -40,24 +61,58 @@ public class TransformAssert {
         return transformAssertWithTransformer;
     }
 
+    /**
+     * Declares the xslt file to be tested<br>
+     * Prints xslt transformation output to standard output
+     * @param xsltFile the xslt {@link File}
+     * @param logBack custom {@link String} {@link Consumer} for log messages
+     * @return instance of {@link TransformAssertWithTransformer}
+     * @throws TransformerException when the xslt cannot be parsed by Saxon
+     */
     public static TransformAssertWithTransformer describe(File xsltFile, Consumer<String> logBack)
             throws TransformerException {
 
         return describe(xsltFile, logBack, null);
     }
 
+    /**
+     * Declares the xslt {@link String} to be tested<br>
+     * Prints xslt transformation output to standard output
+     * @param xslt the xslt {@link String}
+     * @param logBack custom {@link String} {@link Consumer} for log messages
+     * @return instance of {@link TransformAssertWithTransformer}
+     * @throws UnsupportedEncodingException when the character set of the xslt {@link String} ia not supported
+     * @throws TransformerException when the xslt cannot be parsed by Saxon
+     */
     public static TransformAssertWithTransformer describe(String xslt, Consumer<String> logBack)
             throws UnsupportedEncodingException, TransformerException {
 
         return describe(xslt, logBack, null);
     }
 
+    /**
+     * Declares the xslt file to be tested<br>
+     * Logs messages to standard output<br>
+     * Prints xslt transformation output to standard output
+     * @param xsltFile the xslt {@link File}
+     * @return instance of {@link TransformAssertWithTransformer}
+     * @throws TransformerException when the xslt cannot be parsed by Saxon
+     */
     public static TransformAssertWithTransformer describe(File xsltFile)
             throws TransformerException {
 
         return describe(xsltFile, System.out::println);
     }
 
+    /**
+     * Declares the xslt {@link String} to be tested<br>
+     * Logs messages to standard output<br>
+     * Prints xslt transformation output to standard output
+     * @param xslt the xslt {@link String}
+     * @return instance of {@link TransformAssertWithTransformer}
+     * @throws UnsupportedEncodingException when the character set of the xslt {@link String} ia not supported
+     * @throws TransformerException when the xslt cannot be parsed by Saxon
+     */
     public static TransformAssertWithTransformer describe(String xslt)
             throws UnsupportedEncodingException, TransformerException {
         return describe(xslt, System.out::println);
